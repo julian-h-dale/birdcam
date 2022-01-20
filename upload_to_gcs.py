e3fc7d2ab866fb55d6850e25127ad8acaba4d8ff
Julian Dale <julian.h.dale@gmail.com>
2022-01-20 17:22:10 -0600

    updated the FileUploader to handle recieving a path

--- upload_to_gcs.py
+++ upload_to_gcs.py
@@ -3,18 +3,19 @@ from google.cloud import storage
 class FileUploader:
     PATH_TO_CREDS = './creds/credentials.json'
     BUCKET_NAME = 'birdcam-storage'
-    DIRECTORY = "./pics/"
 
     def __init__(self):
         storage_client = storage.Client.from_service_account_json(self.PATH_TO_CREDS)
         self.bucket = storage_client.get_bucket(self.BUCKET_NAME)
 
     def upload(self, filename):
-        blob = self.bucket.blob(filename)
-        blob.upload_from_filename(self.DIRECTORY+filename)
+        # extract the blob name
+        blob_name = filename.split('/')[-1]
+        blob = self.bucket.blob(blob_name)
+        blob.upload_from_filename(filename)
         print("uploaded: " + blob.public_url)
 
 
 if __name__ == '__main__':
     uploader = FileUploader()
-    uploader.upload('test.txt')
+    uploader.upload('./pics/test.txt')
