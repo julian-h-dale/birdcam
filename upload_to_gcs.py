94bfb2016bfae067dc6b9aa1c071261ebd257f6c
Julian Dale <julian.h.dale@gmail.com>
2022-01-20 16:45:14 -0600

    upload of files to gcs works

--- upload_to_gcs.py
+++ upload_to_gcs.py
@@ -3,8 +3,8 @@ from google.cloud import storage
 PATH_TO_CREDS = './creds/credentials.json'
 BUCKET_NAME = 'birdcam-storage'
 FILE_NAME = 'test.txt'
-storage_client = storage.Client.from_service_account(PATH_TO_CREDS)
-bucket = storage_client.get(BUCKET_NAME)
+storage_client = storage.Client.from_service_account_json(PATH_TO_CREDS)
+bucket = storage_client.get_bucket(BUCKET_NAME)
 blob = bucket.blob(FILE_NAME)
-blob.upload('./pics/test.txt')
+blob.upload_from_filename('./pics/test.txt')
 print('uploaded file: ' + blob.public_url)
