b2348e1533904523c5726bd96b6e754ab861462d
Julian Dale <julian.h.dale@gmail.com>
2022-01-20 17:05:28 -0600

    cleaning up code base

--- upload_to_gcs.py
+++ upload_to_gcs.py
@@ -1,18 +1,10 @@
 from google.cloud import storage
 
-# PATH_TO_CREDS = './creds/credentials.json'
-# BUCKET_NAME = 'birdcam-storage'
-# FILE_NAME = 'test.txt'
-# storage_client = storage.Client.from_service_account_json(PATH_TO_CREDS)
-# bucket = storage_client.get_bucket(BUCKET_NAME)
-# blob = bucket.blob(FILE_NAME)
-# blob.upload_from_filename('./pics/test.txt')
-# print('uploaded file: ' + blob.public_url)
-
 class FileUploader:
     PATH_TO_CREDS = './creds/credentials.json'
     BUCKET_NAME = 'birdcam-storage'
     DIRECTORY = "./pics/"
+
     def __init__(self):
         storage_client = storage.Client.from_service_account_json(self.PATH_TO_CREDS)
         self.bucket = storage_client.get_bucket(self.BUCKET_NAME)
